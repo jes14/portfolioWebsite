7cf316f0b4e34ba1a1729072fc51076bf2a76f5b
Yujin <yujin.kim@bergwerk.io>
2021-03-15 09:05:25 +0100

    Fix type

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Yuin'}</title>
+        <title>{title || 'Yujin'}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
