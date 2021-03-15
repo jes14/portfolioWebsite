a7f250f2bace2558efcb21a5297ed26434d93de9
Yujin <yujin.kim@bergwerk.io>
2021-03-15 07:37:55 +0100

    Remove not used lines

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
-import About from './About/About';
 import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
@@ -25,7 +24,6 @@ function App() {
   return (
     <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
       <Hero />
-      {/* <About /> */}
       <Footer />
     </PortfolioProvider>
   );
